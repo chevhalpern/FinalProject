abae1b8f927852eb78e14805c23a403d01a6ecb6
Yocheved Halpern <155897998+chevhalpern@users.noreply.github.com>
2024-04-14 13:11:49 +0000

    4-14 changes

--- app.py
+++ app.py
@@ -70,18 +70,78 @@ def add():
                         db.session.add(Gedolim(f'{data[0][row]}', f'{data[1][row]}', f'{data[2][row]}'))
                 db.session.commit()
                 return redirect(url_for("home"))
+# def random():
+#         with app.app_context():
+#                 gedolim = db.session.query(Gedolim).all()
+#                 randomNumber = random.randint(0, len(gedolim) - 1)
+#                 randomPhotoObject = gedolim[randomNumber]
+#                 randomImage = re.findall("//upload.+?(?=-https)", str(randomPhotoObject))
+#         return randomImage
+
 
 @app.get("/")
 def home():
-        gedolim = db.session.query(Gedolim).all()
-        return render_template("base.html", gedolim=gedolim)
+        # gedolim_image = db.session.query(Gedolim).all()
+        gedolim_image = "//upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Vilna_Gaon_authentic_portrait.JPG/87px-Vilna_Gaon_authentic_portrait.JPG"
+        return render_template("base.html", gedolim=gedolim_image)
 
-# @app.route('/image-render/')
+# def generate(){
+#                 array = [//upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Vilna_Gaon_authentic_portrait.JPG/87px-Vilna_Gaon_authentic_portrait.JPG,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Yitzchak_Zev_Soloveitchik.jpg/96px-Yitzchak_Zev_Soloveitchik.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/0/02/Medini_1.jpg/91px-Medini_1.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/3/38/Shlomo_Ganzfried.jpg/86px-Shlomo_Ganzfried.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Chaim_Yosef_David_Azulai.jpg/88px-Chaim_Yosef_David_Azulai.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Meir_Shapiro.jpg/83px-Meir_Shapiro.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/6/62/Schneur_Zalman_of_Liadi.jpg/96px-Schneur_Zalman_of_Liadi.jpg,
+#                 //upload.wikimedia.org/wikipedia/en/thumb/4/45/Steipler_Gaon.jpg/85px-Steipler_Gaon.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/0/02/Yitzchak_Elchanan_Spektor.jpg/85px-Yitzchak_Elchanan_Spektor.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Shmuel_Salant.jpg/84px-Shmuel_Salant.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Avraham_Mattisyahu_of_Shtefanesht.JPG/91px-Avraham_Mattisyahu_of_Shtefanesht.JPG,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/1/19/Reb_Chaim_Ozer.jpg/92px-Reb_Chaim_Ozer.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Yisrael_Meir_Kagan.jpg/94px-Yisrael_Meir_Kagan.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Rabbi_Kalischer.jpg/108px-Rabbi_Kalischer.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Gershon_Edelshtein.jpg/77px-Gershon_Edelshtein.jpg,
+#                 //upload.wikimedia.org/wikipedia/commons/thumb/3/30/Reb_Moshe_Feinstein.jpg/120px-Reb_Moshe_Feinstein.jpg
+#                 ]
+                
+#         place = randomNum()
+#         let image = document.getElementById('image')
+#         image.setAttribute('src', array[place])
+#         print(array[place])
+#         }
+#         function randomNum(){
+#                 randomNumber = Math.floor(Math.random() * 17);
+#                 return randomNumber;
+#                 }
+
+# @app.get("/generate/<int:src>")
+# def generate(src):
+#         # image = db.session.query(Gedolim).filter(Gedolim.img_src == src).first()
+#         # new_image = random_gadol()
+#         return redirect(url_for("home"))
+
+
+# @app.route('/print_items')
+# def print_items():
+#     cursor = db.execute('SELECT img_src FROM Gedolim')
+#     return render_template('print_items.html', items=cursor.fetchall())
+
 # def random_gadol():
 #         with app.app_context():
 #                 obj = Gedolim.query.all()
 #                 randomNumber = random.randint(0, len(obj) - 1)
 #                 randomPhotoObject = obj[randomNumber]
 #                 randomImage = re.findall("//upload.+?(?=-https)", str(randomPhotoObject))
-#         return redirect(url_for("home"))
+#         return randomImage
+
+# class RandomChoice():
+#         with app.app_context():
+#                 obj = Gedolim.query.all()
+#                 randomNumber = random.randint(0, len(obj) - 1)
+#                 randomPhotoObject = obj[randomNumber]
+#                 print(randomPhotoObject)
+
+
+#         randomImage = re.findall("//upload.+?(?=-https)", str(randomPhotoObject))
+# return randomImage
 
--- testing.py
+++ testing.py
@@ -21,3 +21,79 @@ print(image)
                         // document.getElementById("btnID").style.display = "none"; --> -->
                     <!-- } -->
                 </py-script>
+
+
+
+
+
+
+
+
+
+                <script>
+                fun(){
+                    print();
+                }
+            </script>
+            <!-- <script>
+                function generate()
+                {
+                    // function randomNum(){
+                    //     let randomNum = Math.floor(Math.random * (array.length - 1));
+                    //     return randomNum;
+                    // }
+                    function generateRandomImage(array){
+                        let image = document.getElementById("image");
+                        // number = randomNum();
+                        image.setAttribute("src", array[0]);
+                    };
+                    const imageArray = [
+                        "//upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Vilna_Gaon_authentic_portrait.JPG/87px-Vilna_Gaon_authentic_portrait.JPG",
+                        "//upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Yitzchak_Zev_Soloveitchik.jpg/96px-Yitzchak_Zev_Soloveitchik.jpg",
+                        "//upload.wikimedia.org/wikipedia/commons/thumb/0/02/Medini_1.jpg/91px-Medini_1.jpg"
+                    ];
+                    // const button = document.getElementById("button");
+                    // window.onload = () => generateRandomImage(imageArray);
+                    // button.addEventListener("click", () => generateRandomImage(imageArray));
+                    return generateRandomImage(imageArray);
+                };
+            </script> -->
+            <!-- <script>
+                function generate(){
+                   const imageArray = [
+                    "//upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Vilna_Gaon_authentic_portrait.JPG/87px-Vilna_Gaon_authentic_portrait.JPG",
+                "//upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Yitzchak_Zev_Soloveitchik.jpg/96px-Yitzchak_Zev_Soloveitchik.jpg",
+            "//upload.wikimedia.org/wikipedia/commons/thumb/0/02/Medini_1.jpg/91px-Medini_1.jpg"];
+            const image = document.getElementById("image");
+            const button = document.getElementById("button");
+            window.onload = () => generateRandomImage(imageArray);
+            button.addEventListener("click", () => generateRandomImage(imageArray));
+
+            function generateRandomImage(array){
+                let randomNum = Math.floor(Math.random * array.length);
+                image.setAttribute("src", array[randomNum])
+                }
+                }
+            </script> -->
+
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5TgD-NycF2Odr6Tc1YGzsXN_AF6-AsYM8dq0E7IqNkw&s"
+
+
+
+            array = ["//upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Vilna_Gaon_authentic_portrait.JPG/87px-Vilna_Gaon_authentic_portrait.JPG",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Yitzchak_Zev_Soloveitchik.jpg/96px-Yitzchak_Zev_Soloveitchik.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/0/02/Medini_1.jpg/91px-Medini_1.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/3/38/Shlomo_Ganzfried.jpg/86px-Shlomo_Ganzfried.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Chaim_Yosef_David_Azulai.jpg/88px-Chaim_Yosef_David_Azulai.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Meir_Shapiro.jpg/83px-Meir_Shapiro.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/6/62/Schneur_Zalman_of_Liadi.jpg/96px-Schneur_Zalman_of_Liadi.jpg",
+                 "//upload.wikimedia.org/wikipedia/en/thumb/4/45/Steipler_Gaon.jpg/85px-Steipler_Gaon.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/0/02/Yitzchak_Elchanan_Spektor.jpg/85px-Yitzchak_Elchanan_Spektor.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Shmuel_Salant.jpg/84px-Shmuel_Salant.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Avraham_Mattisyahu_of_Shtefanesht.JPG/91px-Avraham_Mattisyahu_of_Shtefanesht.JPG",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/1/19/Reb_Chaim_Ozer.jpg/92px-Reb_Chaim_Ozer.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Yisrael_Meir_Kagan.jpg/94px-Yisrael_Meir_Kagan.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Rabbi_Kalischer.jpg/108px-Rabbi_Kalischer.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Gershon_Edelshtein.jpg/77px-Gershon_Edelshtein.jpg",
+                 "//upload.wikimedia.org/wikipedia/commons/thumb/3/30/Reb_Moshe_Feinstein.jpg/120px-Reb_Moshe_Feinstein.jpg"
+                 ]
