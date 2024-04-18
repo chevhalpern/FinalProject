e622f9aa788f617a8c28daf4e96b3fb8c88cf42a
Yocheved Halpern <155897998+chevhalpern@users.noreply.github.com>
2024-04-18 04:41:52 +0000

    final (could use filter edits)

--- app.py
+++ app.py
@@ -4,20 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 import re
 from bs4 import BeautifulSoup
 import requests
-import sqlite3
 import random
-
 from sqlalchemy.sql import func
 
+# SET UP FLASK AND DATABASE
 app = Flask(__name__)
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
         'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
 
 # INITIALIZE DATABASE PROPERTIES
@@ -74,31 +70,26 @@ def add():
                 db.session.commit()
                 return redirect(url_for("home"))
 
-# INITIALIZE HTML AND SEND PARAMETER
+# INITIALIZE HTML AND SEND PARAMETER FROM GADOL CLASS
 @app.get("/")
 def home():
         gadol = Gadol()
+        gadol.setUpGadol()
         return render_template("base.html", gedolim=gadol)
-
-# @app.get("/refresh/<int:gadol_id>")
-# def refresh(gadol_id):
-#         gadol = db.session.query(Gedolim).filter(Gedolim.id == gadol_id).first()
-#         new_gadol = Gadol()
-#         if new_gadol.id != gadol.id:
-#                 return redirect(url_for("home"))
-
 
 # CREATE CLASS AND INITIALIZE PROPERTIES
 class Gadol:
-        def randomChoice1(target):
+
+        # METHOD TO SEND ADDITIONAL RANDOM CHOICE #1
+        def randomChoice1(self, target):
                 with app.app_context():
                         gedolim = db.session.query(Gedolim).all()
                         while True:
                                 randomNumber = random.randint(0, len(gedolim) - 1)
                                 if randomNumber != target:
                                         return randomNumber                            
-
-        def randomChoice2(gadol, choice1):
+        # METHOD TO SEND ADDITIONAL RANDOM CHOICE #2
+        def randomChoice2(self, gadol, choice1):
                 with app.app_context():
                         gedolim = db.session.query(Gedolim).all()
                         while True:
@@ -107,8 +98,8 @@ class Gadol:
                                         if randomNumber != choice1:
                                                 return randomNumber
                                         
-        
-        def randomizeChoices(correct, option1, option2):
+        # METHOD TO RANDOMIZE CHOICES SO NOT ALWAYS IN THE SAME ORDER
+        def randomizeChoices(self, correct, option1, option2):
                 choices = ["placeholder1", "placeholder2", "placeholder3"]
                 place1 = random.randint(0, 2)
                 while True:
@@ -124,34 +115,42 @@ class Gadol:
                 choices[place2] = option1
                 choices[place3] = option2
                 return choices
-                                        
-        with app.app_context():
-                gedolim = db.session.query(Gedolim).all()
-                randomNumber = random.randint(0, len(gedolim) - 1)
-                randomPhotoObject = gedolim[randomNumber]
-                link = re.findall("http.+?(?=>)", str(randomPhotoObject))
-                link = "".join(link)
-                source = re.findall("//upload.+?(?=-https)", str(randomPhotoObject))
-                source = "".join(source)
-                name = re.findall("Student.+?(?=-//)", str(randomPhotoObject))
-                id = randomNumber + 1
-                name = ("".join(name)).replace(f"Student {randomNumber + 1}-", "")
-                randomNum1 = randomChoice1(randomNumber)
-                optObject1 = gedolim[randomNum1]
-                choice1 = re.findall("Student.+?(?=-//)", str(optObject1))
-                choice1 = ("".join(choice1)).replace(f"Student {randomNum1 + 1}-", "")
-                randomNum2 = randomChoice2(randomNumber, randomNum1)
-                optObject2 = gedolim[randomNum2]
-                choice2 = re.findall("Student.+?(?=-//)", str(optObject2))
-                choice2 = ("".join(choice2)).replace(f"Student {randomNum2 + 1}-", "")
-                choices = randomizeChoices(name, choice1, choice2)
 
-        def __init__(self, gadol_id=id, source_name=source, gadol_name=name, choices_array=choices, gadol_link=link):
-                self._gadol_id = gadol_id
-                self._source_name = source_name
-                self._gadol_name = gadol_name
-                self._choices_array = choices_array
-                self._gadol_link = gadol_link
+        # SETS UP VALUES TO SEND TO GADOL CONSTRUCTOR
+        def setUpGadol(self):
+                with app.app_context():
+                        gedolim = db.session.query(Gedolim).all()
+                        randomNumber = random.randint(0, len(gedolim) - 1)
+                        randomPhotoObject = gedolim[randomNumber]
+                        link = re.findall("http.+?(?=>)", str(randomPhotoObject))
+                        link = "".join(link)
+                        source = re.findall("//upload.+?(?=-https)", str(randomPhotoObject))
+                        source = "".join(source)
+                        name = re.findall("Student.+?(?=-//)", str(randomPhotoObject))
+                        id = randomNumber + 1
+                        name = ("".join(name)).replace(f"Student {randomNumber + 1}-", "")
+                        randomNum1 = self.randomChoice1(randomNumber)
+                        optObject1 = gedolim[randomNum1]
+                        choice1 = re.findall("Student.+?(?=-//)", str(optObject1))
+                        choice1 = ("".join(choice1)).replace(f"Student {randomNum1 + 1}-", "")
+                        randomNum2 = self.randomChoice2(randomNumber, randomNum1)
+                        optObject2 = gedolim[randomNum2]
+                        choice2 = re.findall("Student.+?(?=-//)", str(optObject2))
+                        choice2 = ("".join(choice2)).replace(f"Student {randomNum2 + 1}-", "")
+                        choices = self.randomizeChoices(name, choice1, choice2)
+                        self._gadol_id = id
+                        self._source_name = source
+                        self._gadol_name = name
+                        self._choices_array = choices
+                        self._gadol_link = link
+
+        # EMPTY CONSTUCTOR ENSURES VALUES WILL CHANGE EACH TIME
+        def __init__(self):
+                self._gadol_id = 0
+                self._source_name = "src"
+                self._gadol_name = "rabbi"
+                self._choices_array = ["rabbi", "rabbi1", "rabbi2"]
+                self._gadol_link = "https://www.rabbi.com"
 
         @property
         def gadol_id(self):
